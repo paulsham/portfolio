f5f94235328f6eb37e39deb5383507e9d4805e53
Paul Sham <contact@paulsham.com>
2013-12-20 03:32:37 -0500

    Breakdown circles.

--- app/js/main.js
+++ app/js/main.js
@@ -25,24 +25,63 @@
   //   $('#logo').removeClass('hover');
   // }
   // var initLoadTimer = setTimeout(initLoad, 2000)
+
+  var setSquare = function (el, val) {
+    var widthInPixels = parseInt(val);
+    el.css({
+      'width': val,
+      'height': val,
+      'marginTop': -val/2
+    })
+  };
+  var positionElements = function (el, prevEl, offset) {
+    var prevWidth = 0;
+
+    prevEl.each(function () {
+      prevWidth = prevWidth + $(this).width() + offset;
+    });
+    el.css({
+      left: prevWidth
+    })
+    .parent().css({
+      width: prevWidth+el.width()
+    });
+  };
+  // var showDescription = function () {
+  //   var description = $(this).nextUntil('dt').first().text();
+  //   var descriptionPos = parseInt($(this).parent('dl').css('left'));
+  //   //$(this).closest('div').next('div').children('p')
+  //   $('#breakdown-circle-description')
+  //   .css({
+  //       'left': descriptionPos,
+  //       'width': $(this).width()
+  //   }).text(description)
+  //   .parent('div').addClass('show-description');
+  // }
+
   /* blur siblings */
   var blurSiblings = function () {
-    $(this).siblings('li').children().addClass('breakdown-circle-blur');
+    $(this).siblings('dl').children('dt').addClass('breakdown-circle-blur');
   };
   var unblurSiblings = function () {
-    $(this).siblings('li').children().removeClass('breakdown-circle-blur');
+    $(this).siblings('dl').children('dt').removeClass('breakdown-circle-blur');
   };
 
+  var $breakdownCircles = $('#intro-breakdown').find('dl');
+  /* position elements */
+  $breakdownCircles.each(function(){
+    var $dl = $(this);
+    var breakdownCirclesDimension = $(this).children('dd').last().text();
 
-
-  /* scrolling event bindings */
-  $(window).on('scroll', updateScrollPos);
-  $(window).on('scroll', addRemoveBodyScrollClass);
-
+    setSquare($dl, breakdownCirclesDimension);
+    positionElements($dl, $dl.prevAll('dl'), -10);
+  });
   /* blur siblings - event binding */
-  $('.breakdown-circle').parent('li').on({
+  $breakdownCircles.on({
     mouseenter: blurSiblings,
     mouseleave: unblurSiblings
+    //mousedown: showDescription
+  });
 
   $('#logo').children('a').smoothScroll({
     offset: -125
